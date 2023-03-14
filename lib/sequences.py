90bcceba5e0e4a4b671cef8af9ed379d8a0a34ec
Alexander <alex.galev@yahoo.com>
2023-03-14 10:52:14 -0600

    solved fibonacci in python!

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,13 @@
 
 def print_fibonacci(length):
     pass
+    result_list = list()
+    counter = 0
+    for n in range(length):
+        result_list.append(counter)
+        if counter == 0:
+            counter = 1
+        else:
+            counter=result_list[n]+result_list[n-1]
+        
+    print(result_list)
